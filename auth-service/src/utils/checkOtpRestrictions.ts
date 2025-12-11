9b85ab73c1f605cc245e254909de354ea548d90d
shakibalfurkan <shakibalfurkan@gmail.com>
2025-12-11 20:31:46 +0600

    auth-service: checkOtpRestrictions method added

--- auth-service/src/utils/checkOtpRestrictions.ts
+++ auth-service/src/utils/checkOtpRestrictions.ts
@@ -1,7 +1,34 @@
 import type { NextFunction } from "express";
+import { redis } from "../database/redis.js";
+import AppError from "../errors/AppError.js";
 
 const checkOtpRestrictions = async (email: string, next: NextFunction) => {
-  // Implementation of OTP restriction checks goes here
+  if (await redis.get(`otp_lock:${email}`)) {
+    return next(
+      new AppError(
+        429,
+        "Account locked due to multiple failed OTP attempts. Please try again after 15 minutes later."
+      )
+    );
+  }
+
+  if (await redis.get(`otp_spam_lock:${email}`)) {
+    return next(
+      new AppError(
+        429,
+        "Too many OTP requests. Please try again after 30 minutes later."
+      )
+    );
+  }
+
+  if (await redis.get(`otp_cooldown:${email}`)) {
+    return next(
+      new AppError(
+        429,
+        "Please wait for 2 minutes before requesting a new OTP."
+      )
+    );
+  }
 };
 
 export default checkOtpRestrictions;
