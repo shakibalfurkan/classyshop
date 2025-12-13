4acd2ec2ba015d1fba88dbb381827586ef1f913b
shakibalfurkan <shakibalfurkan@gmail.com>
2025-12-13 23:53:46 +0600

    Remove .next from tracking

--- user-frontend-ui/.next/dev/server/chunks/ssr/_next-internal_server_app_(AuthLayout)_login_page_actions_27c51e94.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/(AuthLayout)/login/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_%28AuthLayout%29_login_page_actions_27c51e94.js.map
--- user-frontend-ui/.next/dev/server/chunks/ssr/_next-internal_server_app_(MainLayout)_page_actions_ec08b32b.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/(MainLayout)/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_%28MainLayout%29_page_actions_ec08b32b.js.map
