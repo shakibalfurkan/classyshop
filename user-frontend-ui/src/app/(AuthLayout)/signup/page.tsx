06c8e8c7ea90bdb481636a73a1ef0b8ea86f63f5
shakibalfurkan <shakibalfurkan@gmail.com>
2025-12-15 20:07:11 +0600

    user-frontend-ui:add signup page

--- user-frontend-ui/src/app/(AuthLayout)/signup/page.tsx
+++ user-frontend-ui/src/app/(AuthLayout)/signup/page.tsx
@@ -1,14 +1,178 @@
-import type { Metadata } from "next";
+"use client";
 
-export const metadata: Metadata = {
-  title: "ClassyShop | Signup",
-  description: "Welcome to ClassyShop. Your one stop shop for all things.",
+import GoogleLogin from "@/components/shared/auth/GoogleLogin";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { SeparatorWithText } from "@/components/ui/separator";
+import { signupSchema } from "@/schemas/auth.schema";
+import { AlertCircleIcon } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import { Controller, set, useForm } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type TFormData = {
+  name: string;
+  email: string;
+  password: string;
 };
 
-export default function Signup() {
+export default function Singup() {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [serverError, setServerError] = useState<string | null>(null);
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<TFormData>({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: TFormData) => {
+    const name = data.name;
+    const email = data.email;
+    const password = data.password;
+
+    console.log(name, email, password);
+  };
+
   return (
-    <section>
-      <h1>This is Signup component</h1>
+    <section className="max-w-screen-2xl mx-auto p-4">
+      <div>
+        <Link href="/" className="hover:cursor-pointer text-2xl font-semibold">
+          ClassyShop
+        </Link>
+      </div>
+      <div className="flex flex-col items-center justify-center min-h-[90vh] w-full gap-8 ">
+        <div className="text-center">
+          <h1 className="text-3xl font-semibold mt-2.5 mb-4">
+            Sign Up your account
+          </h1>
+          <p className="font-medium text-gray-500">
+            To use ClassyShop, Please enter your details.
+          </p>
+        </div>
+        <div className="w-full max-w-md border rounded-lg p-6">
+          {serverError && (
+            <Alert
+              variant="destructive"
+              className="mb-5 border-red-500 bg-red-50"
+            >
+              <AlertCircleIcon />
+              <AlertTitle>Unable to sing up.</AlertTitle>
+              <AlertDescription>
+                <p>{serverError}</p>
+              </AlertDescription>
+            </Alert>
+          )}
+          <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <Controller
+                name="name"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="name">Name</FieldLabel>
+                    <Input
+                      {...field}
+                      id="name"
+                      type="name"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Enter your name"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name="email"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="email">Email</FieldLabel>
+                    <Input
+                      {...field}
+                      id="email"
+                      type="email"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Enter your email"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+
+              <div className="relative">
+                <Controller
+                  name="password"
+                  control={control}
+                  render={({ field, fieldState }) => (
+                    <Field data-invalid={fieldState.invalid}>
+                      <FieldLabel htmlFor="password">Password</FieldLabel>
+                      <Input
+                        {...field}
+                        id="password"
+                        type={isPasswordVisible ? "text" : "password"}
+                        aria-invalid={fieldState.invalid}
+                        placeholder="Password"
+                      />
+                      {fieldState.invalid && (
+                        <FieldError errors={[fieldState.error]} />
+                      )}
+                    </Field>
+                  )}
+                />
+                <p
+                  onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                  className={`absolute  ${
+                    errors.password ? "top-[42%]" : "top-[60%]"
+                  } right-3 cursor-pointer text-lg`}
+                >
+                  {isPasswordVisible ? <FaEyeSlash /> : <FaEye />}
+                </p>
+              </div>
+            </FieldGroup>
+
+            <Button type="submit" className="w-full hover:cursor-pointer">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex items-center justify-center gap-2 mt-3 mb-5">
+            <span className="text-sm font-medium text-gray-500">
+              Already have an account?
+            </span>
+            <Link
+              href="/login"
+              className="font-medium text-primary text-sm hover:underline"
+            >
+              Sign In
+            </Link>
+          </div>
+          <SeparatorWithText>Or continue with</SeparatorWithText>
+
+          <div className="flex justify-center mt-4">
+            <GoogleLogin />
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- user-frontend-ui/src/schemas/auth.schema.ts
+++ user-frontend-ui/src/schemas/auth.schema.ts
@@ -16,3 +16,28 @@ export const loginSchema = z.object({
     .max(20, { error: "Password must be less than 20 characters" }),
   rememberMe: z.boolean().optional(),
 });
+
+export const signupSchema = z.object({
+  name: z
+    .string({
+      error: (issue) =>
+        issue.input === undefined ? "Name is required." : "Invalid name.",
+    })
+    .min(2, { error: "Name must be 2 characters long" })
+    .trim(),
+
+  email: z.email({
+    error: (issue) =>
+      issue.input === undefined ? "Email is required." : "Invalid email.",
+  }),
+  password: z
+    .string({
+      error: (issue) =>
+        issue.input === undefined
+          ? "Password is required."
+          : "Invalid password.",
+    })
+    .min(8, { error: "Password must be 8 characters long" })
+    .max(20, { error: "Password must be less than 20 characters" })
+    .trim(),
+});
